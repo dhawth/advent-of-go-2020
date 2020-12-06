53af3098ac0b14cd0eec3506a460d21fd39343e9
dhawth <dhawth@devnull.org>
2020-12-05 23:31:55 -0600

    problems 11 and 12

---
+++ cmd/problem11/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/dhawth/advent-of-go-2020/lib"
+	"log"
+)
+
+const (
+	inputFile = "input.txt"
+)
+
+func main() {
+	lines, err := lib.ReadFile(inputFile)
+	if err != nil {
+		log.Fatalf("error reading file: %v", err)
+	}
+
+	var groups []map[rune]struct{}
+
+	v := map[rune]struct{}{}
+
+	for _, line := range lines {
+		if line == "" {
+			groups = append(groups, v)
+			v = map[rune]struct{}{}
+			continue
+		}
+
+		for _, r := range line {
+			v[r] = struct{}{}
+		}
+	}
+
+	groups = append(groups, v)
+	total := 0
+
+	for _, m := range groups {
+		total += len(m)
+
+	}
+
+	log.Printf("Total: %d", total)
+}
+
---
+++ cmd/problem12/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"github.com/dhawth/advent-of-go-2020/lib"
+	"log"
+)
+
+const (
+	inputFile = "input.txt"
+)
+
+type group struct {
+	numMembers int
+	yesses     map[rune]int
+}
+
+func main() {
+	lines, err := lib.ReadFile(inputFile)
+	if err != nil {
+		log.Fatalf("error reading file: %v", err)
+	}
+
+	var groups []group
+
+	v := map[rune]int{}
+	numMembers := 0
+
+	for _, line := range lines {
+		if line == "" {
+			groups = append(groups, group{
+				numMembers: numMembers,
+				yesses:     v,
+			})
+
+			v = map[rune]int{}
+			numMembers = 0
+
+			continue
+		}
+
+		for _, r := range line {
+			n, found := v[r]
+			if found {
+				v[r] = n + 1
+			} else {
+				v[r] = 1
+			}
+		}
+
+		numMembers++
+	}
+
+	groups = append(groups, group{
+		numMembers: numMembers,
+		yesses:     v,
+	})
+
+	total := 0
+
+	for _, g := range groups {
+		for _, n := range g.yesses {
+			if n == g.numMembers {
+				total++
+			}
+		}
+	}
+
+	log.Printf("Total: %d", total)
+}
